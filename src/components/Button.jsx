2785ce0330a2b4f307fa2e055870dd8f0c933c42
walidChaybi <98161047+walidChaybi@users.noreply.github.com>
2023-04-02 16:38:40 +0200

    added the form and realtime validation

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+function Button({ type, children, version, isDisabled }) {
+  return (
+    <button className={`btn btn-${version}`} type={type} disabled={isDisabled}>
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  version: "primary",
+  type: "button",
+  isDisabled: false,
+};
+
+export default Button;
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import Card from "./Card";
+import Button from "./Button";
 function FeedbackForm() {
-  return <div></div>;
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [disabled, setDisabled] = useState(true);
+  const [message, setMessage] = useState("at least 10 caractere is required");
+
+  const handleText = (e) => {
+    setText(e.target.value);
+    console.log(text);
+    if (text.length > 10) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate us?</h2>
+        <div className="input-group">
+          <input
+            onChange={handleText}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button isDisabled={disabled} type="submit">
+            Send
+          </Button>
+        </div>
+        <h3>{disabled && message}</h3>
+      </form>
+    </Card>
+  );
 }
 
 export default FeedbackForm;
